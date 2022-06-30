54046db3cdebceae7a29383850e4744deadfb39a
Thinh Do <dqthinh69752@gmail.com>
2022-07-01 00:10:13 +0700

    UI
    Minor update for UI

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,9 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
+    [SerializeField]
+    TextMeshPro inStockText;
+
+    [SerializeField]
+    TextMeshPro redCountText, greenCountText, blueCountText, yellowCountText;
+
+    [SerializeField]
+    GameObject gameOverScreen;
+
+    [SerializeField]
+    TextMeshPro winnerText;
+
+    [SerializeField]
+    Button spawnButton, gateButton;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +31,18 @@ public class UIManager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void UpdateInStockText(int countInStock)
+    {
+        inStockText.SetText("In Stock: " + countInStock);
+    }
+
+    public void UpdateColorCountText(int redCount, int blueCount, int greenCount, int yellowCount)
+    {
+        redCountText.SetText("Red: " + redCount);
+        blueCountText.SetText("Blue: " + blueCount);
+        greenCountText.SetText("Green: " + greenCount);
+        yellowCountText.SetText("Yellow: " + yellowCount);
     }
 }
